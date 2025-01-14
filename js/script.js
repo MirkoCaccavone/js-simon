83e47f9c61405b637c33fd95ae72ef9d1a661f7b
Mirko <mirkocaccavone03@gmail.com>
2025-01-14 18:18:57 +0100

    [feat] add function to check answers

--- js/script.js
+++ js/script.js
@@ -7,26 +7,13 @@
 // selezione elementi di output
 const outputCountDown = document.getElementById('countdown');
 const randomNumber = document.getElementById('numbers-list');
-
+const outputMessage = document.getElementById('message');
 
 // selezione elementi di input
-
+const form = document.getElementById('answers-form');
+const numField = document.querySelectorAll('input');
 
 // FUNZIONI
-
-// countdown
-let second = 30;
-
-countDown = setInterval(() => {
-
-    if(second === 0){
-        clearInterval(countDown);
-
-    }else{
-        outputCountDown.innerHTML = --second;
-
-    }  
-},1000);
 
 // generazione numeri casuali
 let numbers = [];
@@ -41,7 +28,60 @@ for(let i = 0; i < 5; i++){
     randomNumber.appendChild(newItems);
 
 };
-
 console.log(numbers);
+
+// countdown
+let second = 5;
+
+countDown = setInterval(() => {
+
+    if(second === 0){
+        clearInterval(countDown);
+        // quando il countdown è a 0 nascondo i numeri
+        randomNumber.innerHTML = '';
+        // mostro il form
+        form.classList.remove('d-none');
+
+    }else{
+        outputCountDown.innerHTML = --second;
+
+    }  
+},1000);
+
+// controllo le risposte 
+form.addEventListener('submit', (event) => {
+    // blocca il refresh
+    event.preventDefault();
+
+    let userAnswers = [];
+
+    for(let i = 0; i < numField.length; i++){
+
+        // cicla e aggiunge il num iesimo all'array userAnswers
+        userAnswers.push(parseInt(numField[i].value));
+    };
+
+    console.log(userAnswers);
+
+    let correctAnswers = [];
+
+    for(let i = 0; i < numbers.length; i++){
+
+        // se un nun iesimo dell'utente e presente nei numeri casuali creati
+        if(numbers.includes(userAnswers[i])){
+
+        // cicla e aggiunge il num iesimo all'array correctAnswers
+            correctAnswers.push(userAnswers[i]);
+        };
+    };
+
+    console.log(correctAnswers);
+
+    outputMessage.innerHTML = `Hai indovinato ${correctAnswers.length} numeri su 5. (${correctAnswers.join('-')})`;
+
+});
+
+
+
 
 
